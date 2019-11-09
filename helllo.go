0d5699db3af94908829f9e22f2ab63bf73aa198a
web1992 <554933654@qq.com>
2019-11-09 23:11:00 +0800

    init

---
+++ helllo.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+
+	var s int = 1
+	println(s)
+
+}
---
+++ test/var-test.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := 1
+	fmt.Println(s)
+}
