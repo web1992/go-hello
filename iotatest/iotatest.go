78ed35740f17c105ab879bb08eb5119a815d00e0
web1992 <554933654@qq.com>
2019-11-30 21:21:15 +0800

    add iota test

---
+++ iotatest/iotatest.go
@@ -0,0 +1,23 @@
+package iotatest
+
+type Month int
+
+const (
+	January Month = 1 + iota
+	February
+	March
+	April
+	May
+	June
+	July
+	August
+	September
+	October
+	November
+	December
+)
+
+// IotaTest iota test
+func IotaTest() {
+	println("December is", December)
+}
