b1fc4e1f12bc56d1ef8ebf62c9cf0fdfb54d6244
web1992 <554933654@qq.com>
2020-03-21 00:31:37 +0800

    fix reflect

--- reflecttest/reflecttest.go
+++ reflecttest/reflecttest.go
@@ -15,7 +15,8 @@ func Reflect() {
 	fmt.Println(reflect.TypeOf(1))
 	fmt.Println(reflect.TypeOf(3.3))
 	fmt.Println(reflect.TypeOf("asd"))
-
+	fmt.Println(reflect.TypeOf(float64(22)))
+	fmt.Println(reflect.TypeOf(float32(22)))
 	person := new(Person)
 
 	fmt.Println(reflect.TypeOf(person))
