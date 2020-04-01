5d70913f71d8d7a88dfe73767ebfb6d1ed06bbce
web1992 <554933654@qq.com>
2020-04-01 13:08:43 +0800

    add array test

--- arraytest/arraytest.go
+++ arraytest/arraytest.go
@@ -11,16 +11,28 @@ func TestArray() {
 	fmt.Println("arr2", arr2)
 	fmt.Println("arr2[1]", arr2[1])
 
-	arr3 := [3]*int{1: new(int), 2: new(int)}
-	fmt.Println("arr3", arr3)
+	arr3 := [3]*int{1: new(int), 2: new(int), 0: new(int)}
+	fmt.Println("arr3", arr3, "arr3 len is", len(arr3))
+	fmt.Println("*arr3[1] is", *arr3[1])
+	fmt.Println("*arr3[2] is", *arr3[2])
+	fmt.Println("*arr3[0] is", *arr3[0])
 	*arr3[1] = 1
 	*arr3[2] = 100
-	fmt.Println("arr3[1]", *arr3[1])
-	fmt.Println("arr3[2]", *arr3[2])
+	fmt.Println("*arr3[1]", *arr3[1])
+	fmt.Println("*arr3[2]", *arr3[2])
 
 	arr4 := [5]int{11, 22, 33, 44, 55}
 
 	for index, v := range arr4 {
-		fmt.Println("index", index, "value", v)
+		fmt.Print("index=", index, ",", "value=", v, "\t")
 	}
+
+	fmt.Println()
+
+	arr5 := [3]string{}
+	arr5[0] = "a"
+	arr5[1] = "b"
+	arr5[2] = "c"
+	//arr5[3]="c"
+	fmt.Println("arr5 is", arr5)
 }
