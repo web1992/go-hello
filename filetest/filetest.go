7f9c2e0b56a979a98ce81fb4b7484248f1598345
web1992 <554933654@qq.com>
2020-07-17 13:48:51 +0800

    add file down

--- filetest/filetest.go
+++ filetest/filetest.go
@@ -1,20 +1,29 @@
 package filetest
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
+var dir string
+var port int
+var staticHandler http.Handler
+
 var (
 	// 文件 key
 	uploadFileKey = "upload-key"
+	uploadDir     = "./down/"
 )
 
-func Main() {
+func main() {
+
 	http.HandleFunc("/upload", uploadHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	http.HandleFunc("/", staticServer)
+
+	if err := http.ListenAndServe(":5549", nil); err != nil {
 		log.Fatalf("error to start http server:%s", err.Error())
 	}
 }
@@ -27,7 +36,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Printf("selected file name is %s", header.Filename)
 	// 将文件拷贝到指定路径下，或者其他文件操作
-	dst, err := os.Create(header.Filename)
+	dst, err := os.Create(uploadDir + header.Filename)
 	if err != nil {
 		// ignore
 	}
@@ -35,4 +44,25 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		// ignore
 	}
+}
+
+// 初始化参数
+func init() {
+	//dir = path.Dir(os.Args[0])
+	//flag.Parse()
+	//flag.IntVar(&port, "port", 5549, "服务器端口")
+	//dir = path.Dir(uploadDir)
+	fmt.Println("dir:", http.Dir(dir))
+	staticHandler = http.FileServer(http.Dir(dir))
+}
+
+// 静态文件处理
+func staticServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("path:" + req.URL.Path)
+	if req.URL.Path != "/down/" {
+		staticHandler.ServeHTTP(w, req)
+		return
+	}
+
+	io.WriteString(w, "hello, world!\n")
 }
--- filetest/filetest_test.go
+++ filetest/filetest_test.go
@@ -2,7 +2,7 @@ package filetest
 
 import "testing"
 
-// curl --form "upload-key=@go.mod" http://127.0.0.1:8080/upload
+// curl --form "upload-key=@go.mod" http://127.0.0.1:5549/upload
 func TestFile(t *testing.T) {
-	Main()
+	main()
 }
