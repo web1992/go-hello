5de4f49e4b5ad79cde7d889f0dbe54bce26d83c6
web1992 <554933654@qq.com>
2020-07-31 16:41:39 +0800

    add comment

--- txttest/textet_test.go
+++ txttest/textet_test.go
@@ -2,6 +2,7 @@ package txttest
 
 import "testing"
 
+// for i in `cat r.txt |awk -F "\r" '{print $1}' `;do  echo $i; curl $i>>resp.txt; echo "\n" >> resp.txt ;done;
 func TestCurl(t *testing.T) {
 	Curl()
 }
