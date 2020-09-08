635ebbf4e10e73553021bd01fb1561c0fd34872e
web1992 <554933654@qq.com>
2020-09-08 22:12:38 +0800

    add combiningt.go

--- combiningtest/combiningt.go
+++ combiningtest/combiningt.go
@@ -1,1 +1,34 @@
 package combiningtest
+
+import "fmt"
+
+type notifier interface {
+	notify()
+}
+
+type user struct {
+	name  string
+	email string
+}
+
+type admin struct {
+	user
+	level string
+}
+
+type admin2 struct {
+	level string
+	user  user
+}
+
+func (u user) notify() {
+	fmt.Println("Notify by user name=", u.name, " email=", u.email)
+}
+
+func (a admin2) notify() {
+	fmt.Println("Notify by admin2 name=", a.user.name, " email=", a.user.email)
+}
+
+func SendNotify(n notifier) {
+	n.notify()
+}
--- combiningtest/combiningt_test.go
+++ combiningtest/combiningt_test.go
@@ -2,6 +2,14 @@ package combiningtest
 
 import "testing"
 
-func TestConstTest(t *testing.T) {
+func TestAdmin(t *testing.T) {
 
+	u := user{email: "554933654@qq.com", name: "Lucy"}
+
+	a := admin{u, "100"}
+	a2 := admin2{"100", u}
+
+	SendNotify(u)
+	SendNotify(a)
+	SendNotify(a2)
 }
