730616b0f1c78d114ac9c110ca2634eb85b35823
web1992 <554933654@qq.com>
2020-09-22 20:24:57 +0800

    fix method_receive_test

--- methodtest/method_receive_test.go
+++ methodtest/method_receive_test.go
@@ -11,6 +11,10 @@ func Test11(t *testing.T) {
 func TestReceive2(t *testing.T) {
 
 	var r = rectangle{2, 3}
+	// 下面加上了 shape 就和报错
+	// var rs shape = rectangle{2, 3}
+	// fmt.Println(typeof(rs))
+
 	fmt.Printf("%T \n", r)
 	fmt.Println(typeof(r))
 	r.area()
