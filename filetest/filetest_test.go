332c0eb207623c80de0c64363082029d487411ba
web1992 <554933654@qq.com>
2020-09-29 16:04:50 +0800

    fix comment

--- filetest/filetest_test.go
+++ filetest/filetest_test.go
@@ -3,6 +3,7 @@ package filetest
 import "testing"
 
 // curl --form "upload-key=@go.mod" http://127.0.0.1:5549/upload
+// curl --form "upload-key=@go.mod" http://121.36.133.225:5549/upload
 func TestFile(t *testing.T) {
 	main()
 }
