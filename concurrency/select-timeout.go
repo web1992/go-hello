1f4d48612e611bfbfd7d64e718dee4e8c2df2357
web1992 <554933654@qq.com>
2021-03-17 22:32:03 +0800

    add select-timeout.go

---
+++ concurrency/select-timeout.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func boring6(msg string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return ch
+}
+
+func main() {
+
+	time := time.After(3 * time.Second)
+
+	c := boring6("Tom")
+
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
+		case <-time:
+			{
+				fmt.Println("out time")
+				return
+			}
+		}
+	}
+}
