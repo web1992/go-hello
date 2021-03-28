f6431a97d30e133fabbde0a5375ea8c5fa4ee65f
web1992 <554933654@qq.com>
2021-03-28 22:02:45 +0800

    fix goin test

--- goins/goin_static.go
+++ goins/goin_static.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 )
 
+const (
+	p1 = "/Users/zl/Documents/DEV/github/go-hello"
+	p2 = "/Users/zl/Documents/DEV/github/go-hello"
+	p3 = "/Users/zl/Documents/DEV/github/go-hello/arraytest/arraytest.go"
+)
+
 func main() {
 	router := gin.Default()
 
-	router.Static("/file1", "/Users/zl/Documents/DEV/github/go-hello")
-	router.StaticFS("/file3", http.Dir("/Users/zl/Documents/DEV/github/go-hello"))
+	router.Static("/file1", p1)
+	router.StaticFS("/file3", http.Dir(p2))
 
-	router.StaticFile("/file2", "/Users/zl/Documents/DEV/github/go-hello/arraytest/arraytest.go")
+	router.StaticFile("/file2", p3)
 	router.Run(":8082")
 }
